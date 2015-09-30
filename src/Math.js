8891d3b3184e150dc898270ea40ad83b7f9ed0fa
hengwu <youchuangqiba@163.com>
2015-09-30 22:43:32 +0800

    update: vector in Math.

--- src/Math.js
+++ src/Math.js
@@ -9,46 +9,55 @@ var Vector = (function () {
         // check arguments:
         // todo
 
-        this._date = [];
+        this._data = [];
     }
         .method('front', function () {
             if(this.isEmpty()) return null;
-            return this._date[0];
+            return this._data[0];
         })
         .method('back', function () {
             if(this.isEmpty()) return null;
-            return this._date[this._date.length - 1];
+            return this._data[this._data.length - 1];
         })
         .method('size', function () {
-            return this._date.length;
+            return this._data.length;
         })
         .method('isEmpty', function () {
             return this.size() == 0;
         })
         .method('push', function (v) {
-            this._date.push(v);
+            this._data.push(v);
             return this;
         })
         .method('rpush', function (v) {
-            this._date.unshift(v);
+            this._data.unshift(v);
             return this;
         })
         .method('pop', function () {
-            this._date.pop();
+            this._data.pop();
             return this;
         })
         .method('rpop', function () {
-            this._date.shift();
+            this._data.shift();
             return this;
         })
         .method('each', function (callback) {
-            for(var i = 0, len = this._date.length; i < len; i++) {
-                callback(this._date[i]);
+            for(var i = 0, len = this._data.length; i < len; i++) {
+                callback(this._data[i]);
             }
             return this;
         })
         .method('subset', function () {})
-        .method('add', function () {})
+        .method('add', function (vec, elementAdd) {
+            if(this.size() != vec.size()) {
+                throw new Error('Different sizes.');
+                return this;
+            }
+            for(var i = 0, len = this.size(); i < len; i++) {
+                this._data[i] = elementAdd ? elementAdd(this._data[i], vec[i]) : (this._data[i] + vec[i]);
+            }
+            return this;
+        })
         .method('multiply', function () {})
         .method('power', function () {})
         .method('sqrt', function () {})
@@ -66,7 +75,7 @@ var Matrix = (function () {
         // check arguments:
         // todo
 
-        this._date = [];
+        this._data = [];
     }
         .method('frontRow', function () {
             // todo
@@ -81,7 +90,7 @@ var Matrix = (function () {
             // todo
         })
         .method('size', function () {
-            return [this._date.length, this._date.length == 0 ? 0 : this._date[0].length];
+            return [this._data.length, this._data.length == 0 ? 0 : this._data[0].length];
         })
         .method('isEmpty', function () {
             // todo
