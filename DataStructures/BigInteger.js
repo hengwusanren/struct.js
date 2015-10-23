88f2ec80d39ae0b0d84049a90c7c33de9a8a1b85
hengwu <youchuangqiba@163.com>
2015-10-23 16:19:17 +0800

    update: BigInt.

--- DataStructures/BigInteger.js
+++ DataStructures/BigInteger.js
@@ -10,23 +10,36 @@ var BigInteger = (function () {
         this.positive = value >= 0;
         //this._initialCapacity = 32;
         //this._capacity = this._initialCapacity;
-        this.radix = 16;
+        this.radix = 16; // an element in this.data ranges from 0 to (2^16 - 1).
         this.data = new Array();
     }
         .method('load', function (value) {
-            if(value < 0) {
-                this.positive = false;
-                value = -value;
-            }
+            var r = this.radix;
+            this.positive = value >= 0;
+            if(value < 0) value = -value;
             while(value > 0) {
-                this.data.push(value ^ ((value >> 16) << 16));
-                value >> 16;
+                this.data.push(value ^ ((value >> r) << r));
+                value >> r;
             }
         })
         .method('isZero', function () {
             // todo
         })
+        .method('_shrink', function () {
+            while(this.data.length > 0 && this.data[this.data.length - 1] == 0) this.data.pop();
+        })
         .method('add', function (v) {
+            var len1 = this.data.length,
+                len2 = v.data.length,
+                bonus = 0,
+                arr = [],
+                r = this.radix;
+            for(var i = 0; i < len1 || i < len2; i++) {
+                var tmp = (this.data[i] ? this.data[i] : 0) + (v.data[i] ? v.data[i] : 0) + bonus;
+                arr.push(tmp ^ ((tmp >> r) << r));
+                bonus = (tmp >> r);
+            }
+            if(bonus > 0) arr.push(bonus);
             // todo
         })
         .method('subtract', function (v) {
