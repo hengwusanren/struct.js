9dcffc43ca26c30e9d7466966020cde831fcc7c6
hengwu <youchuangqiba@163.com>
2015-11-28 22:50:03 +0800

    update: UnionFindSet.

--- DataStructures/UnionFindSet.js
+++ DataStructures/UnionFindSet.js
@@ -19,41 +19,15 @@ var UnionFindSet = (function (SuperHashMap, SuperHashSet) {
         if(!lazy) this.s2e = new SuperHashMap();
     }
         .method('put', function (elem, setEntry) { // tobe fixed
-            if(setEntry == null) return null;
-            if(this.s2e) {
-                var oldSet = this.e2s.get(elem);
-                if(oldSet != null) this.s2e.get(oldSet).remove(elem);
-            }
-            this.e2s.put(elem, setEntry);
-            if(this.s2e) {
-                var oldElements = this.s2e.get(setEntry);
-                if(oldElements) oldElements.add(elem);
-                else {
-                    oldElements = new SuperHashSet();
-                    oldElements.add(elem);
-                    this.s2e.put(setEntry, oldElements);
-                }
-            }
-            return this;
+            // todo
         })
-        /**
-         * elem1 to be added is a neighbor of elem2
-         * some problem here: what if an insertion makes two different sets to be united?
-         */
-        .method('putNeighbor', function (elem1, elem2) { // tobe fixed
-            var s = this.e2s.get(elem2);
-            if(s == null) return null;
-            return this.put(elem1, s);
+        .method('join', function (elem1, elem2) { // tobe fixed
+            // todo
         })
-        .method('getSetOf', function (elem) { // tobe fixed
-            return this.e2s.get(elem);
+        .method('find getSetOf', function (elem) { // tobe fixed
+            // todo
         })
         .method('getElementsOf', function (setEntry) { // tobe fixed
-            if(this.s2e) {
-                return this.s2e.get(setEntry);
-            }
-            var elems = [];
-            // todo: traverse this.e2s to get elems
-            return elems;
+            // todo
         });
 })(HashMap, HashSet);
